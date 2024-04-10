7eaaffd3664c19379e24f7a4be3af56606ec25aa
kamzzi <pluzeq@gmail.com>
2024-04-10 18:27:29 +0200

    BST
    Implement a few searching method like BFS,InOrder,
    PostOrder..

--- src/data_structures/tree/tree.ts
+++ src/data_structures/tree/tree.ts
@@ -60,15 +60,96 @@ export class Tree<T> {
 
     return null;
   }
+
+  BFS() {
+    if (!this.root) return;
+
+    const data: TreeNode<T>[] = [];
+    const queue: TreeNode<T>[] = [];
+
+    let node = this.root;
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+
+      data.push(node);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    console.log(data);
+    return data;
+  }
+
+  DFSPreOrder() {
+    if (!this.root) return null;
+
+    const data: TreeNode<T>["value"][] = [];
+
+    let current = this.root;
+
+    const helper = (node: TreeNode<T>) => {
+      data.push(node!.value);
+      if (node.left) helper(node.left);
+      if (node.right) helper(node.right);
+    };
+
+    helper(current);
+    return data;
+  }
+
+  DFSPostOrder() {
+    if (!this.root) return null;
+
+    const data: TreeNode<T>["value"][] = [];
+
+    let current = this.root;
+
+    const helper = (node: TreeNode<T>) => {
+      if (node.left) helper(node.left);
+      if (node.right) helper(node.right);
+      data.push(node!.value);
+    };
+
+    helper(current);
+    return data;
+  }
+
+  DFSInOrder() {
+    if (!this.root) return null;
+
+    const data: TreeNode<T>["value"][] = [];
+
+    let current = this.root;
+
+    const helper = (node: TreeNode<T>) => {
+      if (node.left) helper(node.left);
+      data.push(node!.value);
+      if (node.right) helper(node.right);
+    };
+
+    helper(current);
+    return data;
+  }
 }
 
 const tree = new Tree();
 
 tree.insert(10);
-tree.insert(5);
+tree.insert(6);
 tree.insert(3);
-tree.insert(11);
-tree.insert(7);
-tree.insert(30);
+tree.insert(8);
+tree.insert(15);
+tree.insert(20);
 
-console.log(tree);
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSInOrder());
