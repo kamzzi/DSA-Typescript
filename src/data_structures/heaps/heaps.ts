925dbede1eb34145841c6b051f8584e5e8d16fdb
kamzzi <pluzeq@gmail.com>
2024-04-15 19:05:37 +0200

    MaxBinaryHeap
    Extract Max in binary heap method implementation.

--- src/data_structures/heaps/heaps.ts
+++ src/data_structures/heaps/heaps.ts
@@ -24,6 +24,55 @@ export class MaxBinaryHeap {
       }
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+
+    this.values[0] = end!;
+
+    this.sinkDown();
+
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftIdx = idx * 2 + 1;
+      let rightIdx = idx * 2 + 2;
+
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftIdx < length) {
+        leftChild = this.values[leftIdx];
+        if (leftChild > element) {
+          swap = leftIdx;
+        }
+      }
+
+      if (rightIdx < length) {
+        rightChild = this.values[rightIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild!)
+        ) {
+          swap = rightIdx;
+        }
+      }
+
+      if (!swap) break;
+
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+
+      idx = swap;
+    }
+  }
 }
 
 const mbp = new MaxBinaryHeap();
@@ -31,5 +80,7 @@ const mbp = new MaxBinaryHeap();
 mbp.insert(55);
 
 mbp.insert(13);
+
+mbp.extractMax();
 
 console.log(mbp.values);
